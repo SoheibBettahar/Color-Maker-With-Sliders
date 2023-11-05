864dd845aef74c6f82a7e53210fdc2f00b0ffd68
SoheibBettahar <soheibbett@gmail.com>
2023-11-05 16:10:15 +0100

    create Color Maker With Sliders

--- Color Maker With Sliders/ViewController.swift
+++ Color Maker With Sliders/ViewController.swift
@@ -9,11 +9,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var redSlider: UISlider!
+    @IBOutlet weak var greenSlider: UISlider!
+    @IBOutlet weak var blueSlider: UISlider!
+    @IBOutlet weak var colorView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    
+    @IBAction func changeColor(){
+        
+        
+        
+        let r = redSlider.value
+        let g = greenSlider.value
+        let b = blueSlider.value
+        
+        colorView.backgroundColor = UIColor(red: CGFloat(r), green: CGFloat(g), blue: CGFloat(b), alpha: 1)
+    }
+    
 
 }
 
